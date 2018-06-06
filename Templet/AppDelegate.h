6d4ab55a3b0f19174d09af6e2b048c018ca7ea8c
EEvvee <609862262@qq.com>
2018-06-06 20:34:08 +0800

    Initial Commit

---
+++ Templet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Templet
+//
+//  Created by 丁一明 on 2018/6/6.
+//  Copyright © 2018年 丁一明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
